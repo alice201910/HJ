8aa471be6fc6346f86ed27ffd3f44b1b2eea0d71
alice201909 <alice201909@gmail.com>
2019-10-18 18:54:52 +0900

    commit

---
+++ HJ/src/main/java/org/zerock/HelloTests.java
@@ -0,0 +1,20 @@
+package org.zerock;
+
+import org.junit.Test;
+
+/**
+ * HelloTest
+ */
+public class HelloTests {
+     //test 할 때만 이 라이브러리 쓰기 때문에 scope 지워주기
+    @Test
+    public void test1(){
+
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+    }
+    
+}
