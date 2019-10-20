7ea2458ddbcde434562bf2b8377efee25ec66c9d
HJ <rosealice201910@gmail.com>
2019-10-21 01:51:26 +0900

    commit

---
+++ HJ/src/HelloServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * HelloServlet
+ */
+@WebServlet("/hello")
+
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            out.write("Hello world");
+    }
+}
